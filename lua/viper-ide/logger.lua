ba59aabfd9c98aeb9807b7d159153e27206d0c02
Timo Früh <dev@tifrueh.ch>
2026-05-23 16:59:38 +0200

    Add logger module

---
+++ lua/viper-ide/logger.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+local function format(msg, level_str)
+    return "[ViperIDE][" .. level_str .. "] " .. msg
+end
+
+M.trace = function (msg)
+    if M.current_level <= vim.log.levels.TRACE then
+        vim.notify(format(msg, "TRACE"), vim.log.levels.TRACE)
+    end
+end
+
+M.debug = function (msg)
+    if M.current_level <= vim.log.levels.DEBUG then
+        vim.notify(format(msg, "DEBUG"), vim.log.levels.DEBUG)
+    end
+end
+
+M.info = function (msg)
+    if M.current_level <= vim.log.levels.INFO then
+        vim.notify(format(msg, "INFO"), vim.log.levels.INFO)
+    end
+end
+
+M.warn = function (msg)
+    if M.current_level <= vim.log.levels.WARN then
+        vim.notify(format(msg, "WARN"), vim.log.levels.WARN)
+    end
+end
+
+M.error = function (msg)
+    if M.current_level <= vim.log.levels.ERROR then
+        vim.notify(format(msg, "ERROR"), vim.log.levels.ERROR)
+    end
+end
+
+return M
