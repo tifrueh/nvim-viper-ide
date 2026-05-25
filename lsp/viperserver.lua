cad464f30f88618ca5ae898d0b780bd683e3d1e1
Timo Früh <dev@tifrueh.ch>
2026-05-25 22:22:31 +0200

    Set capabilities explicitly

--- lsp/viperserver.lua
+++ lsp/viperserver.lua
@@ -1,5 +1,6 @@
 return {
     cmd = require('viper-ide.cmd'),
+    capabilities = require('viper-ide.capabilities'),
     filetypes = { 'viper' },
     root_markers = { '.git' },
     on_attach = function ()
