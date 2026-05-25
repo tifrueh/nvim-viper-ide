89426b5b56290837b0dc8c77363801064abcc750
Timo Früh <dev@tifrueh.ch>
2026-05-25 22:19:49 +0200

    Handle stdout/stderr of ViperServer

--- lua/viper-ide/server.lua
+++ lua/viper-ide/server.lua
@@ -3,6 +3,21 @@ local logger = require("viper-ide.logger")
 
 local M = {}
 
+local stdout_buf = ""
+local stderr_buf = ""
+
+local function flush_stdout ()
+    if stdout_buf == "" then return end
+    logger.debug("[ViperServer][stdout] " .. stdout_buf)
+    stdout_buf = ""
+end
+
+local function flush_stderr ()
+    if stderr_buf == "" then return end
+    logger.warn("[ViperServer][stderr] " .. stderr_buf)
+    stderr_buf = ""
+end
+
 local function start()
     state.viperserver_obj = vim.system(
         {
@@ -10,7 +25,23 @@ local function start()
             '--serverMode', 'LSP',
             '--singleClient', '--port', tostring(state.viperserver_port)
         },
-        { text = true },
+        {
+            text = true,
+            stdout = function (err, data)
+                if err then return end
+                if data then
+                    stdout_buf = stdout_buf .. data
+                    vim.schedule(flush_stdout)
+                end
+            end,
+            stdin = function (err, data)
+                if err then return end
+                if data then
+                    stderr_buf = stderr_buf .. data
+                    vim.schedule(flush_stderr)
+                end
+            end
+        },
         function ()
             state.viperserver_state = state.se.STOPPED
             logger.warn("ViperServer exited …")
