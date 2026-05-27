98317d7520666b2b8e905a98744cdaf262dc212b
Timo Früh <dev@tifrueh.ch>
2026-05-27 14:22:06 +0200

    Improve logger module
    Always defer the notification, so that the logger functions can be used
    in all circumstances.

--- lua/viper-ide/logger.lua
+++ lua/viper-ide/logger.lua
@@ -8,40 +8,47 @@ M.ps = {
 }
 
 local current_level = vim.lsp.log.get_level()
+local level_str = {
+    [vim.log.levels.TRACE] = "TRACE",
+    [vim.log.levels.DEBUG] = "DEBUG",
+    [vim.log.levels.INFO]  = "INFO",
+    [vim.log.levels.WARN]  = "WARN",
+    [vim.log.levels.ERROR] = "ERROR",
+}
+local level_buffer = {
+    [vim.log.levels.TRACE] = {},
+    [vim.log.levels.DEBUG] = {},
+    [vim.log.levels.INFO] = {},
+    [vim.log.levels.WARN] = {},
+    [vim.log.levels.ERROR] = {},
+}
+
 
 local function format(msg, level_str)
     return "[ViperIDE][" .. level_str .. "] " .. msg
 end
 
-M.trace = function (msg)
-    if current_level <= vim.log.levels.TRACE then
-        vim.notify(format(msg, "TRACE"), vim.log.levels.TRACE)
+local function flush(level)
+    for _,b in ipairs(level_buffer[level]) do
+        vim.notify(b, level)
+    end
+    level_buffer[level] = {}
+end
+
+local function create_writer(level)
+    return function (msg)
+        if current_level <= level then
+            table.insert(level_buffer[level], format(msg, level_str[level]))
+            vim.schedule(function () flush(level) end)
+        end
     end
 end
 
-M.debug = function (msg)
-    if current_level <= vim.log.levels.DEBUG then
-        vim.notify(format(msg, "DEBUG"), vim.log.levels.DEBUG)
-    end
-end
-
-M.info = function (msg)
-    if current_level <= vim.log.levels.INFO then
-        vim.notify(format(msg, "INFO"), vim.log.levels.INFO)
-    end
-end
-
-M.warn = function (msg)
-    if current_level <= vim.log.levels.WARN then
-        vim.notify(format(msg, "WARN"), vim.log.levels.WARN)
-    end
-end
-
-M.error = function (msg)
-    if current_level <= vim.log.levels.ERROR then
-        vim.notify(format(msg, "ERROR"), vim.log.levels.ERROR)
-    end
-end
+M.trace = create_writer(vim.log.levels.TRACE)
+M.debug = create_writer(vim.log.levels.DEBUG)
+M.info = create_writer(vim.log.levels.INFO)
+M.warn = create_writer(vim.log.levels.WARN)
+M.error = create_writer(vim.log.levels.ERROR)
 
 M.progress = function (msg, percent, status)
     vim.api.nvim_echo(
